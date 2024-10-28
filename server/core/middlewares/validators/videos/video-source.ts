4594fa9aeb49a4e33d942dd109ac127169a9bb5d
nieve <ppu7haxj@duck.com>
2024-10-28 02:42:44 -0400

    Update video-source.ts

--- server/core/middlewares/validators/videos/video-source.ts
+++ server/core/middlewares/validators/videos/video-source.ts
@@ -26,7 +26,7 @@ export const videoSourceGetLatestValidator = [
     const video = getVideoWithAttributes(res) as MVideoFullLight
 
     const user = res.locals.oauth.token.User
-    //if (!checkUserCanManageVideo(user, video, UserRight.UPDATE_ANY_VIDEO, res)) return
+    if (!checkUserCanManageVideo(user, video, UserRight.UPDATE_ANY_VIDEO, res)) console.log("aaaaaaaaaaa");
 
     res.locals.videoSource = await VideoSourceModel.loadLatest(video.id)
 
